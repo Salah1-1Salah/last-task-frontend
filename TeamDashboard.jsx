69aa1ad644ffda868584b2752b7146ba05acc67e
Salah Salah <nagibnagib041@gmail.com>
2025-06-29 20:59:08 +0100

    Fix: added React import to all JSX files

--- TeamDashboard.jsx
+++ TeamDashboard.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import React, { useState, useEffect } from 'react';
 
 const API_URL = import.meta.env.VITE_API_URL;
--- main.jsx
+++ main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 
 
 console.log("✅ VITE_API_URL:", import.meta.env.VITE_API_URL);
